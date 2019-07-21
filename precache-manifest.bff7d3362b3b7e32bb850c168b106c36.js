aa952d989e14208bf0bd2b64879adb78f97d87eb
Valarie Cardoza <vjcardoza2011@yahoo.com>
2019-07-20 20:04:48 -0500

    Updates

---
+++ precache-manifest.bff7d3362b3b7e32bb850c168b106c36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c90b331c8171ed79a3b1916a313d5b0",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "e570117fc323b8373fcc",
+    "url": "/Clicky-Game/static/css/main.9a0a909b.chunk.css"
+  },
+  {
+    "revision": "9634a2804d5597ac62b3",
+    "url": "/Clicky-Game/static/js/2.548e0644.chunk.js"
+  },
+  {
+    "revision": "e570117fc323b8373fcc",
+    "url": "/Clicky-Game/static/js/main.927c8ff2.chunk.js"
+  },
+  {
+    "revision": "fab82da4d9420c34bc57",
+    "url": "/Clicky-Game/static/js/runtime~main.012bace8.js"
+  },
+  {
+    "revision": "ba8c53917849139d915c529380d5fae5",
+    "url": "/Clicky-Game/static/media/TWD-background.ba8c5391.jpg"
+  }
+]);
